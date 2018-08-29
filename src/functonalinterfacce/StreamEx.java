f469e014a7352b3f9f8d7eeef76f105794d92ad1
dell <vashok915@gmail.com>
2018-08-29 13:28:20 +0530

    Javaprojects

--- src/functonalinterfacce/StreamEx.java
+++ src/functonalinterfacce/StreamEx.java
@@ -16,9 +16,23 @@ public class StreamEx {
         al.add(3);
         Stream<Integer> s=al.stream();
 
-        s.forEach(m->{
+        //s.forEach(m->{
+          //  System.out.println(m);
+        //});
+
+        System.out.println("Filter");
+        s.filter(x-> x>2).forEach(m->{
             System.out.println(m);
         });
+
+        ArrayList<String> al1=new ArrayList<>();
+        al1.add("one");
+        al1.add("two");
+        al1.add("three");
+        al1.stream().filter(e->e.contains("t")).forEach(q->{
+            System.out.println(q);
+        });
+
 
     }
 }
